d4ca25e66e9fd705c2567062a879d2fb2a4c5ef2
Joao Pedro <jpremedi@hotmail.com>
2020-12-14 12:07:58 -0300

    exercícios JS aula 6

--- ex-004/js/funcoes.js
+++ ex-004/js/funcoes.js
@@ -2,4 +2,6 @@ function MetrosParaCentimetros() {
     let metros = document.getElementById("metros").value;
     let centimetros = metros * 1000;
     document.getElementById("resultado").innerText = "Centimetros: " + centimetros;
+
+    document.getElementById("metros").value = "";
 }
---
+++ ex-005/js/funcoes.js
@@ -0,0 +1,7 @@
+function MetrosParaPolegadas() {
+    let metros =  document.getElementById("metros").value;
+    let polegadas = metros * 39.3701;
+    document.getElementById("resultado").innerText = "Polegadas: " + polegadas.toFixed(0);
+
+    document.getElementById("metros").value = "";
+}
